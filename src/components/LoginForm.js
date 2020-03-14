1b48b3a093438a7957068ae86d29c5058e0ab288
Mari <demarius.edwards@gmail.com>
2020-03-14 01:28:18 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,29 +1,48 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  state = {
+    username: "",
+    password: ""
+  }
 
-    this.state = {};
+  
+  handleAllInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  
+  
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (!this.state.username || !this.state.password){
+      return false
+    } else {
+
+      return this.props.handleLogin(this.state)
+    }
   }
 
   render() {
+    console.log(this.props)
+    // let {username, password} = this.props.handleLogin
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleAllInput} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleAllInput} />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" >Log in</button>
         </div>
       </form>
     );
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,31 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  state = {
+    message: "",
+    remainingCharacters: 280
+  }
 
-    this.state = {};
+  handleOnChange = (e) => {
+    let remChar = this.state.remainingCharacters -1
+    this.setState({
+      [e.target.name]: e.target.value,
+      remainingCharacters: remChar
+    })
+  }
+  
+  handleType = (e) => {
+   
   }
 
   render() {
+    console.log(this.state)
+   let {message, remainingCharacters} = this.state
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" value={message} onChange={this.handleOnChange} id="message" />
+        Characters Remaining: {remainingCharacters}
       </div>
     );
   }
